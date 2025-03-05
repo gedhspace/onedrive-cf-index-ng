e442c027b6b0171ed9c4b168e406a43e68a6c0fa
Gedh-StuffyWalk <103632788+gedhspace@users.noreply.github.com>
2025-03-05 19:45:14 +0800

    1111

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Public'],
+  protectedRoutes: ['/图片','/文档','/桌面'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:738813115@qq.com',
